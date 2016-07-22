1128dd0cab132a8832bfc781c9bb9daa988518f3
Benoit Averty <benoit.averty@gmail.com>
2016-07-22 18:07:20 +0200

    fix(file): add missing setup.js

---
+++ test/setup.js
@@ -0,0 +1,22 @@
+import chai from 'chai';
+import sinonChai from 'sinon-chai';
+import chaiEnzyme from 'chai-enzyme';
+import { jsdom } from 'jsdom';
+
+chai.use(sinonChai);
+chai.use(chaiEnzyme());
+
+const exposedProperties = ['window', 'navigator', 'document'];
+
+global.document = jsdom('');
+global.window = document.defaultView;
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    exposedProperties.push(property);
+    global[property] = document.defaultView[property];
+  }
+});
+
+global.navigator = {
+  userAgent: 'node.js',
+};
